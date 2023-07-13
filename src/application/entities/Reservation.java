8bad8da3e4e7d3afbaac581f0fc4bfba962d1c9a
Ricardo <rockobaschirotto@gmail.com>
2023-07-13 08:50:45 -0300

    solução ruim

--- src/application/entities/Reservation.java
+++ src/application/entities/Reservation.java
@@ -10,7 +10,7 @@ public class Reservation {
     private Date checkOut;
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-    
+
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
@@ -29,31 +29,40 @@ public class Reservation {
         return checkIn;
     }
 
-
     public Date getCheckOut() {
         return checkOut;
     }
-    
+
     public long duration() {
         long diff = checkOut.getTime() - checkIn.getTime();
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkIn, Date checkOut) {
+    public String updateDates(Date checkIn, Date checkOut) {
+
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Reservation dates for update must be future dates";
+        }
+        if (!checkOut.after(checkIn)) {
+            return "Check-out date must be after check-in date!";
+        }
+
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null;
     }
 
     @Override
     public String toString() {
         return "Room "
-            + roomNumber
-            + ", check-in: "
-            + sdf.format(checkIn)
-            + ", check-out: "
-            + sdf.format(checkOut)
-            + ", "
-            + duration()
-            + " nights";
+                + roomNumber
+                + ", check-in: "
+                + sdf.format(checkIn)
+                + ", check-out: "
+                + sdf.format(checkOut)
+                + ", "
+                + duration()
+                + " nights";
     }
 }
